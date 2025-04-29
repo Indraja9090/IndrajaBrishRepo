209ea371b42cdfd6886d58060c77615eedd3f4bc
bhavana <bhavana.taduru96@gmail.com>
2025-04-29 16:17:11 +0530

    Call check_reboot from main, exit with 1 on error

---
+++ Scripts/hello.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3      # best line if you're running this file on Git Bash or  Linux too).
+                            #'env' here will search the system’s "PATH" for the correct python3 interpreter and
+                            #                                    run the script with it.
+#!C:/Python313/python.exe   # use this sheband if you want to run from local windows pythonterminal
+
+import os
+import sys
+
+def check_reboot():
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
+
+def main():
+    if check_reboot():
+        print("Pending Reboot.")
+        print("Pending Reboot.")
+        print("Pending Reboot.")
+        sys.exit(1)
+
+main()
