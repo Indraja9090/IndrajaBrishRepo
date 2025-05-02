1d49d0049a2e8a9b96db54aee7af2a411f6953f8
indraja9090 <indrajameruva@gmail.com>
2025-05-02 10:47:43 +0530

    Moved files from subdirectory of project repo to root directory to maintain project structure

--- isPalindrome.js
+++
@@ -1,5 +0,0 @@
-function isPalindrome(str) {
-    const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-    return cleanStr === cleanStr.split('').reverse().join('');
-  }
-  module.exports = isPalindrome;
