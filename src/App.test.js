fd16865a2effbbc0b5c9e14ab02aa33aff7b83dc
indraja9090 <indrajameruva@gmail.com>
2025-05-07 16:08:37 +0530

    Make assertions from before and after event using queryBy and getBy.

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,20 @@
-/* --- change the above test() to make sure that our components handles asynchronous task .*/
+/*---- we can make the assertions from before and after the event----*/
 import * as React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-
 import App from './App';
 describe('App', () => {
-  //Async 
   it('renders App component', async () => {
     render(<App />);
 
-    // Wait for the async user data to load (this handles the act() warning)
+    // wait for the user to resolve
     await screen.findByText(/Signed in as/);
-
-    screen.debug();
-    // Now simulate typing in the input
+    //assertion
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+    //assertion
     fireEvent.change(screen.getByRole('textbox'), {
       target: { value: 'JavaScript' },
     });
 
-    screen.debug();
+    expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
   });
 });
