027db539d61eebb3a2dfaba48aad312983855b9f
indraja9090 <indrajameruva@gmail.com>
2025-05-07 14:58:40 +0530

    it isn't necessary to assign aria roles to HTML elements explicitly for the sake of testing, because the DOM already has implicit roles attached to HTML elements.

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,13 @@
-/*-------------- Like getByText(), Exploring  other search functions (like 'getByRole') in React Testing Library. -----*/
+/*-------------- getByRole() mainly DOM elements ni provide chesins accessible roles base chesi find cheyyadaniki. Idi accessibility standards ni follow chestundi 
+                role should be (string): Ikkada role ante HTML element yokka semantic role. For example:"button","textbox","heading","link","checkbox","radio"-----*/
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
 
-  // screen.debug() // 👈 This shows the rendered HTML in your terminal or the virtual HTML that RTL rendered.
+  expect(screen.getByText(/learn react/i)).toBeInTheDocument();
 
-  // expect(screen.getByText(/learn react/i)).toBeInTheDocument();
-
-  /* The neat thing about 'getByRole', it shows all the selectable roles 
-  if you provide a role that isn't available in the rendered component's HTML*/
-
-  screen.getByRole('');  // TestingLibraryElementError: Unable to find an accessible element with the role ""
+  screen.getByRole('img'); 
 
 });
