8ed82c706e3d4bc94059c81c2e17f1b2e30a97d1
indraja9090 <indrajameruva@gmail.com>
2025-05-07 14:53:34 +0530

    TestingLibraryElementError: Unable to find an accessible element with the role ' '

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,17 @@
-/*-------------- using debug() function on screen object , 
-                Writing Implicit assertion without 'expect()' and Exlicit assertion with 'expect()' jest funcion -----*/
+/*-------------- Like getByText(), Exploring  other search functions (like 'getByRole') in React Testing Library. -----*/
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
 
-  screen.debug() // 👈 This shows the rendered HTML in your terminal or the virtual HTML that RTL rendered.
+  // screen.debug() // 👈 This shows the rendered HTML in your terminal or the virtual HTML that RTL rendered.
 
-  /* Implicit assertion because 'getByText' would throw error if element wouldn't be there even if you don't write expect(...)*/
+  // expect(screen.getByText(/learn react/i)).toBeInTheDocument();
 
-  // screen.getByText(/learn react/i);
+  /* The neat thing about 'getByRole', it shows all the selectable roles 
+  if you provide a role that isn't available in the rendered component's HTML*/
 
-  /* Best recommended practice: write Explicit assertion with jest's expect() function */
-
-  expect(screen.getByText(/learn react/i)).toBeInTheDocument();
+  screen.getByRole('');  // TestingLibraryElementError: Unable to find an accessible element with the role ""
 
 });
