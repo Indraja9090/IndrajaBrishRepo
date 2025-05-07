24030f93202c4bb4af3851f3aea3352743bce30a
indraja9090 <indrajameruva@gmail.com>
2025-05-07 15:59:13 +0530

    You can wait for the user to appear on the screen using findByText (which automatically handles act() internally).

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,18 @@
-/* ------ use RTL's "fireEvent" and "waitFor" functions to simulate interactions of an end user ------*/
-// If a user types into an input field, the component may re-render , and 
-// the new value should be displayed (or used somewhere).
+/* --- change the above test() to make sure that our components handles asynchronous task .*/
 import * as React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+
 import App from './App';
-
-// If your component is involved in an asynchronous task, 
-// you may see the  warning showing up: "Warning: An update to App inside a test was not wrapped in act(...).
-
 describe('App', () => {
-  it('renders App component', () => {
+  //Async 
+  it('renders App component', async () => {
     render(<App />);
 
-    screen.debug();
+    // Wait for the async user data to load (this handles the act() warning)
+    await screen.findByText(/Signed in as/);
 
+    screen.debug();
+    // Now simulate typing in the input
     fireEvent.change(screen.getByRole('textbox'), {
       target: { value: 'JavaScript' },
     });
