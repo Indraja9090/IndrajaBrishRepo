0a0f1026acb239023af68972ce2cf79eb1940569
indraja9090 <indrajameruva@gmail.com>
2025-05-07 17:22:37 +0530

    userEvent

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,24 @@
-/*--- As alternative to use Async and await to test asynchronous task, 
-      we can also literally wait for an asynchronous update to happen with React Testing Library's "waitFor" function*/
-// In this section,
-//       we have used the queryBy search variant to check whether the element isn't there before the event and 
-//                    the getBy search variant to check whether the element there after the event. 
+
+/*----------userEvent mimics the actual browser behavior more closely than the fireEvent API.-----------*/
 import * as React from 'react';
-// waitFor() is a utility from React Testing Library used to wait for asynchronous changes in the DOM to complete before running assertions.
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import App from './App';
 
 describe('App', () => {
-  it('renders App component', () => {
+  it('renders App component', async () => {
     render(<App />);
+
+    // wait for the user to resolve
+    await screen.findByText(/Signed in as/);
 
     expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
 
-    fireEvent.change(screen.getByRole('textbox'), {
-      target: { value: 'JavaScript' },
-    });
-    // Means: “Wait until the text 'Searches for JavaScript appears' in the document.”
-    waitFor(() =>
-      expect(
-        screen.getByText(/Searches for JavaScript/)
-      ).toBeInTheDocument()
-    );
+    await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
+
+    expect(
+      screen.getByText(/Searches for JavaScript/)
+    ).toBeInTheDocument();
   });
 });
